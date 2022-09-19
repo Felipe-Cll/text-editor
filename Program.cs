8054cb0fac3ec19ddcd6bea3d2931bc9b735ecb2
Felipe <FelipeCll01@outlook.com>
2022-09-19 08:56:09 -0300

    First version of the project

---
+++ Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+namespace TextEdit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("O que você deseja fazer? ");
+            Console.WriteLine("1 - Abrir arquivo");
+            Console.WriteLine("2 - Criar novo arquivo");
+            Console.WriteLine("0 - Sair");
+            short option = short.Parse(Console.ReadLine());
+
+            switch(option)
+            {
+                case 0: Exit(); break;
+                case 1: Open(); break;
+                case 2: Edit(); break;
+                default: Invalid(); break;
+            }
+        }
+
+        static void Open()
+        {
+            Console.Clear();
+            Console.WriteLine("Qual caminho do arquivo? ");
+            string path = Console.ReadLine();
+
+            using (var file = new StreamReader(path))
+            {
+                string text = file.ReadToEnd();
+                Console.WriteLine(text);
+            }
+
+            Console.WriteLine("");
+            Console.ReadLine();
+            Menu();
+        }
+
+        static void Edit()
+        {
+            Console.Clear();
+            Console.WriteLine("Digite seu texto abaixo: (ESC para sair)");
+            Console.WriteLine("----------------------------------------");
+            string text = "";
+
+            do 
+            {
+                text += Console.ReadLine();
+                text += Environment.NewLine;
+            }
+            while(Console.ReadKey().Key != ConsoleKey.Escape);
+
+            Save(text);
+        }
+
+        static void Save(string text)
+        {
+            Console.Clear();
+            Console.WriteLine("Qual caminho para salvar o arquivo? ");
+            var path = Console.ReadLine();
+
+            using (var file = new StreamWriter(path))
+            {
+                file.Write(text);
+            }
+
+            Console.WriteLine($"Arquivo {path} salvo com sucesso!");
+            Console.ReadLine();
+            Menu();
+        }
+
+        static void Exit()
+        {
+            Console.Clear();
+            Console.WriteLine("Obrigado por usar nosso aplicativo, volte sempre!");
+            System.Environment.Exit(0);
+        }
+
+        static void Invalid()
+        {
+            Console.WriteLine("Opção inválida!");
+            int delay = 1000;
+            Thread.Sleep(delay);
+            Menu();
+        }
+    }
+}
